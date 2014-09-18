7e143511cb764d6030de449e557cc1df5c8e3ad9
Aileen J Pierce <aileen.pierce@colorado.edu>
2014-09-18 09:04:41 -0600

    tipcalculator lab 4

---
+++ tipcalculator/tipcalctest.playground/section-1.swift
@@ -0,0 +1,10 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+let amount = ("100" as NSString).floatValue
+let pct = ("10" as NSString).floatValue/100
+let numberOfPeople="4".toInt()!
+
+
+
